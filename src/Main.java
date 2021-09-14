4d22600a3807551af2b36c5210b24f72d4088a38
Davide Corsi <davide.corsi@par-tec.it>
2021-09-14 18:03:21 +0200

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
+
 /*
  * Trasformate una lista di stringhe in una mappa dove la chiave sono le stringhe racchiuse tra apici 
- * singoli e il valore è la lunghezza della stringa stessa. Utilizzate gli stream.
+ * singoli e il valore è la lunghezza della stringa stessa, inoltre la mappa deve essere ordinata. 
+ * Utilizzate gli stream.
  */
 public class Main {
 	public static void main(String[] args) {
 		String[] stringArray = new String[] {"gli", "stream", "ti", "permettono", "di", "compattare", "il",
 		"codice"};
-	}
 }
