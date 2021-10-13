fe84739cbf5a879f8ff1cf628a3ba8271a80f3d8
Davide Corsi <davide.corsi@par-tec.it>
2021-10-13 17:54:37 +0200

    risoluzione

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,21 @@
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 import java.util.stream.Collectors;
 
 /*
- * Trasformate una lista di stringhe in una mappa dove la chiave sono le stringhe racchiuse tra apici 
- * singoli e il valore è la lunghezza della stringa stessa, inoltre la mappa deve essere ordinata. 
- * Utilizzate gli stream.
+ * Trasformate questa lista aggiungendo degli apici singoli all'inizio ed alla fine di ogni stringa. 
  */
 public class Main {
 	public static void main(String[] args) {
 		String[] stringArray = new String[] {"gli", "stream", "ti", "permettono", "di", "compattare", "il",
 		"codice"};
 		
-		Map<String, Integer> map = Arrays.stream(stringArray)
-				.collect(Collectors.toMap(s -> "'" + s + "'", String::length, (v1 , v2) -> v1, TreeMap::new));
+		List<String> stringList = Arrays.stream(stringArray)
+				.map(s -> "'" + s + "'")
+				.collect(Collectors.toList());
 		
-		System.out.println(map);
+		stringList.forEach(System.out::println);
 	}
 }
